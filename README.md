1f8097bb435d680ae4ee9ec4a395fcec558ee686
Grigor Hakobyan <grigorhakobyan2007@gmail.com>
2024-08-12 19:11:49 +0400

    linking project file to github repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Very-Simple-Car-Identifier
+a very simple project that tracks cars or similar objects in videos/photos inputted into the program
+I wrote this about a year ago when I was very new to coding and especially python so try not to judge the somewhat crappy code lol!
+
+# How this works
+The program uses the CV2 module to capture and analyze the contrast of every frame and compares it with a library
+
+# How to Customize
+to customize it all you need to do is switch out the file inside the "Media_Analyzed" folder to any video you'd like to be ran by the program
---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+haar_cascade = 'cars.xml'
+video = 'Media_analyzed/example_video.mp4'
+
+capture = cv2.VideoCapture(video) 
+car_cascade = cv2.CascadeClassifier(haar_cascade)
+
+while True: 
+
+    ret, img_frame = capture.read()
+
+    gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)
+
+    cars = car_cascade.detectMultiScale(gray, 1.3, 1)
+
+    for (x,y,w,h) in cars: 
+
+        cv2.rectangle(img_frame, (x,y), (x+w,y+h), (0,255,0), 2) 
+
+    cv2.imshow('example_video.mp4', img_frame)
+
+    if cv2.waitKey(33) == 27:
+       break
+
+cv2.destroyAllWindows()
